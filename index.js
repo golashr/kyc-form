fa12bdb94243984a6b25ff93f3ce9e5bf5b325a8
hqman <hqmank@gmail.com>
2019-11-03 11:27:14 +0800

    fix css font favicon not found

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv').config();
 const db = require('./components/db');
 const express = require('express');
 const helmet = require('helmet')
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const app = express();
 const cors = require('cors');
 const port = process.env.SERVER_PORT || 5002;
@@ -12,6 +12,7 @@ const server = app.listen(port, () => {
   app.engine('handlebars', exphbs());
   app.set('view engine', 'handlebars');
   app.use(helmet());
+  app.use(express.static('public'));
   app.use(express.json());
   app.use(cors());
   app.use('/', router);
