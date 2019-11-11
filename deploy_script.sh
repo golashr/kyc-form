abba91f7d93ccdf7f2b14dfc69a4baea0658dd05
golashr <rahul.g@ledgerium.net>
2019-11-11 18:32:17 +1100

    deployed script

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 echo "Deploying containers"
-echo $PWD
-cd $PWD
+echo "Move to ledgeriumkyc folder"
+cp docker-compose.yml docker-compose.yml_v$BUILDID 
 
+echo "List images"
+docker images 
+
+echo "replace docker image tag and restart the container"
+sed -i /"s//ledgeriumkyc:v1.0//ledgeriumkyc:v${BUILDID}//g/" docker-compose.yml && docker-compose up -d
+
+echo "script is finished"
