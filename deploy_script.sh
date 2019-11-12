f18a0cc03ee86e7f9e67294b5b7bfa8c2fa751af
golashr <rahul.g@ledgerium.net>
2019-11-13 00:14:14 +1100

    renewed

--- deploy_script.sh
+++ deploy_script.sh
@@ -13,13 +13,20 @@ echo "Move to ledgeriumkyc folder"
 cd ~/ledgerium/ledgeriumkyc
 
 echo "Taking backup of the compose file" 
-cp docker-compose.yml docker-compose.yml_v123 
+cp docker-compose.yml docker-compose.yml_$BUILDID 
 
 echo "Pull the container down in case it is running" 
 docker-compose down 
 
+VERSIONSTR=`awk '/ledgeriumkyc:v/{print}' docker-compose.yml`
+VERSIONSTR=${VERSIONSTR:33:12}
+echo $VERSIONSTR
+
+echo "String replace"
+sed -i "s/$VERSIONSTR/v$BUILDID/g" docker-compose.yml
+
 echo "Pull the new image from dockercontainer down in case it is running" 
-docker pull $DOCUSENAME/ledgeriumkyc:vbuild-29996 
+docker pull $DOCUSENAME/ledgeriumkyc:v$BUILDID 
 
 echo "Bringing up the containers" 
 docker-compose up -d
